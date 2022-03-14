bb5727a4d8cca07b41bdae7135cf49519fa7f742
Renato Delboni <renatonato_jr@hotmail.com>
2022-03-14 15:45:13 +0900

    created first useState using Tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState<{
+    id: string,
+    title: string,
+    date: string,
+    text: string,
+    color: string,
+  }[]>([{
+    id: (new Date).toString(),
+    title: 'First Note',
+    text: 'This is the first note',
+    color: '#ff0000',
+    date: (new Date).toString(),
+  }]);
+
   return (
     <div className="App">
       Hello World
